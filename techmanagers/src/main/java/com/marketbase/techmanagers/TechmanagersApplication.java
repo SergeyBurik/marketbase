4b58b170ec02e969006e4aebf3596746bab2ef5e
Sergio Burik <sergioburik@gmail.com>
2020-12-23 20:38:11 +0300

    eureka server

---
+++ techmanagers/src/main/java/com/marketbase/techmanagers/TechmanagersApplication.java
@@ -0,0 +1,13 @@
+package com.marketbase.techmanagers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmanagersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmanagersApplication.class, args);
+	}
+
+}
---
+++ techmanagers/src/test/java/com/marketbase/techmanagers/TechmanagersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketbase.techmanagers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmanagersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
